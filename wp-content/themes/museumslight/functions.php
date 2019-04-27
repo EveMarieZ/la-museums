38d26d78f36a79c57d6d30f8f7a987c9c5d36425
Queen Bavmorda <e.zaunbrecher@gmail.com>
2019-04-27 15:34:35 -0500

    added left sidebar

--- wp-content/themes/museumslight/functions.php
+++ wp-content/themes/museumslight/functions.php
@@ -122,6 +122,8 @@ add_action( 'widgets_init', 'museumslight_widgets_init' );
 function museumslight_scripts() {
 	wp_enqueue_style( 'museumslight-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'sidebar-content', get_template_directory_uri(). '/layouts/sidebar-content.css' );
+
 	wp_enqueue_script( 'museumslight-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'museumslight-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
