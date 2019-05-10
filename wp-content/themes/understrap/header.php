727fe3d9ba5b5db545e024a03e8b3ab540035e5f
Queen Bavmorda <e.zaunbrecher@gmail.com>
2019-05-10 15:21:46 -0500

    child theme edits

--- wp-content/themes/understrap/header.php
+++ wp-content/themes/understrap/header.php
@@ -19,13 +19,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,300i" rel="stylesheet">
-
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-
+<?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
@@ -33,7 +31,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+		<nav class="navbar navbar-expand-md navbar-light">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
