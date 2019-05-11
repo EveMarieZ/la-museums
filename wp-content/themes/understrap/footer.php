e6af563e90cec5f09c9362a31746f868c405cb46
Queen Bavmorda <e.zaunbrecher@gmail.com>
2019-05-10 21:04:13 -0500

    new logos, global Lato font, gulp watch

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -28,7 +28,35 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						<!-- <?php understrap_site_info(); ?> -->
+						<div class="col-sm-3">
+							<ul class="text-uppercase">
+								<li><a href="">About</a></li>
+								<li><a href="">Conference</a></li>
+								<li><a href="">Membership</a></li>
+								<li><a href="">Resources</a></li>
+								<li><a href="">Blog</a></li>
+							</ul>
+						</div>
+						<div class="col-sm-3">
+							<ul class="text-uppercase">
+								<li><a href="">Jobs</a></li>
+								<li><a href="">Museum Directory</a></li>
+								<li><a href="">Contact Us</a></li>
+								<li><a href="">About</a></li>
+							</ul>
+						</div>
+						<div class="col-sm-3">
+							<p class="text-uppercase">Louisiana Association of Museums<br>
+							100 North Blvd<br>
+							Baton Rouge, LA 70801<br>
+							225-383-6800</p>
+						</div>
+						<div class="col-sm-3">
+							<p class="text-uppercase">Copyright &copy; 2019<br>
+							Louisiana Association of Museums <br>
+							<a href="https://www.facebook.com/LAMuseums/">Follow Us on Facebook</a></p>
+						</div>
 
 					</div><!-- .site-info -->
 
