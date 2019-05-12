2ae28d67c3970474459940f70fc4d5f140ce7a31
Queen Bavmorda <e.zaunbrecher@gmail.com>
2019-05-11 23:25:14 -0500

    footer widget addition

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -25,36 +25,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="col-md-12">
 
 				<footer class="site-footer" id="colophon">
-					<div class="footer-sitemap d-flex">
-						<div class="col-md-3">
-							<ul class="text-uppercase footer-sitemap-list">
-								<li><a href="">About</a></li>
-								<li><a href="">Conference</a></li>
-								<li><a href="">Membership</a></li>
-								<li><a href="">Resources</a></li>
-								<li><a href="">Blog</a></li>
-							</ul>
-						</div>
-						<div class="col-md-3">
-							<ul class="text-uppercase footer-sitemap-list">
-								<li><a href="">Jobs</a></li>
-								<li><a href="">Museums Directory</a></li>
-								<li><a href="">Contact Us</a></li>
-							</ul>
-						</div>
-						<div class="col-md-3">
-							<p class="text-uppercase">Louisiana Association of Museums<br>
-							100 North Blvd<br>
-							Baton Rouge, LA 70801<br>
-							225-383-6800</p>
-						</div>
-						<div class="col-md-3">
-							<p class="text-uppercase">Copyright &copy; 2019<br>
-							Louisiana Association of Museums <br>
-							<a href="https://www.facebook.com/LAMuseums/">Follow Us on Facebook</a></p>
-						</div>
-					</div>
-					<div class="col-xs-12 site-info d-flex justify-content-center">
+					<div class="footer-sitemap d-flex justify-content-center">
 
 						<!-- <?php understrap_site_info(); ?> -->
 						<p>Designed & Developed by <span class="color-links"><a href="http://evewebdesign.com">Eve Web Design</a></span><p>
