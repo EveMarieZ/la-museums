6862019972f814424acfb208783ad377e414827c
Queen Bavmorda <e.zaunbrecher@gmail.com>
2019-06-27 22:22:34 -0500

    edited page, header and footer and moved to child theme

---
+++ wp-content/themes/understrap-child/footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<footer class="site-footer" id="colophon">
+					<div class="footer-sitemap d-flex justify-content-center">
+
+						<!-- <?php understrap_site_info(); ?> -->
+						<p>Designed & Developed by <span class="color-links"><a href="http://evewebdesign.com" target="_blank">Eve Web Design</a></span><p>
+					</div>
+
+					</div><!-- .site-info -->
+
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
